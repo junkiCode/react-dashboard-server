dde8373746b9abef91be161a7f9609ddaaa1296e
Ashik-AD <looge321@gmail.com>
2023-01-09 16:09:40 +0545

    add packages & api endpoints

---
+++ app.js
@@ -0,0 +1,21 @@
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import services from "./service/AppService.js";
+import express from "express";
+import morgan from "morgan";
+const app = express();
+const logger = morgan("short");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger);
+services(app);
+const server = http.createServer(app);
+const port = process.env.PORT || 9000;
+server.listen(port, console.log(`Server running on ${port} port`));
+
+app.use("/default", async (_, res) => {
+  res.send("Lol, don't fuck with me");
+});
---
+++ service/AppService.js
@@ -0,0 +1,60 @@
+import email from "./api/emails.json" assert { type: "json" };
+import chats from "./api/chats.json" assert { type: "json" };
+import invocices from "./api/invoice.json" assert { type: "json" };
+import users from "./api/users.json" assert { type: "json" };
+import userProfile from "./api/user.json" assert { type: "json" };
+import pricing from "./api/pricing.json" assert { type: "json" };
+import faq from "./api/faq.json" assert { type: "json" };
+import chartData from "./api/chartData.json" assert { type: "json" };
+import rechartData from "./api/rechartData.json" assert { type: "json" };
+import dashboard from "./api/dashboard.json" assert { type: "json" };
+
+export default function services(app) {
+  app.get("/api/dashboards/:collectionName", async (req, res, next) => {
+    const { collectionName } = req.params;
+    const data = dashboard[collectionName];
+    res.send(data);
+  });
+  app.get("/api/emails/", async (req, res) => {
+    res.send(email);
+  });
+  app.get("/api/chat/:collectionName", async (req, res) => {
+    const { collectionName } = req.params;
+    res.send(chats[collectionName]);
+  });
+  app.get("/api/invoices/", async (req, res) => {
+    res.end(invocices);
+  });
+
+  app.get("/api/users/:collectionName", async (req, res) => {
+    const { collectionName } = req.params;
+    res.send(users[collectionName]);
+  });
+  app.get("/api/user_profile/:collectionName", async (req, res) => {
+    const { collectionName } = req.params;
+    res.send(userProfile[collectionName]);
+  });
+
+  app.get("/api/pricing/:collectionName", async (req, res) => {
+    const { collectionName } = req.params;
+    res.send(pricing[collectionName]);
+  });
+  app.get("/api/faq/", () => faq);
+
+  app.get("/api/charts/:type", async (req, res) => {
+    const nodeName = req.params.type;
+    res.send(chartData[nodeName]);
+  });
+
+  app.get("/api/rechart/:type", async (req, res) => {
+    const nodeName = req.params.type;
+    res.send(rechartData[nodeName]);
+  });
+
+  app.use((err, req, res, next) => {
+    console.log(err);
+    res
+      .status(500)
+      .send({ message: "Lol, your requsted api dosen't exist!!!" });
+  });
+}
